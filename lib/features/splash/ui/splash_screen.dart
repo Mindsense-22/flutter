82c8e53b0c7d7e93c1827a8fa370e28c5230b6bc
yousifmohamed74 <joe94574@gmail.com>
2025-12-01 01:31:55 +0200

    splash_screen

--- lib/features/splash/ui/splash_screen.dart
+++ lib/features/splash/ui/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mindsense_app/features/login/ui/login_screen.dart';
 import 'package:mindsense_app/features/on%20boarding/ui/onboarding_screens.dart';
 
@@ -13,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();    
-    Future.delayed(Duration(seconds: 1)).then((value) {
+    Future.delayed(Duration(seconds: 3)).then((value) {
       Navigator.pushAndRemoveUntil(
         // ignore: use_build_context_synchronously
         context,
@@ -25,11 +26,25 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Image.asset("assets/images/onp1.jpg",
-          fit: BoxFit.fill,
-          height: 160,
-          width: 160,    
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 15,
+            children: [
+              Image.asset(
+                "assets/images/logo.png",
+                width: double.infinity,
+                height: 275.h,
+              ),
+              //SizedBox(height: 24.h,),
+              Text("MindSense",style: TextStyle(
+                fontSize: 32.sp,
+                fontWeight: FontWeight.bold,                
+              ),)
+            ],
+          ),
         ),
       ),      
     );
