9a1253d82008b282c8517a838cd766cbab60b7ef
yousifmohamed74 <joe94574@gmail.com>
2026-03-29 06:13:59 +0200

    inhance cache

--- lib/features/Analyzing/logic/analyzing_provider.dart
+++ lib/features/Analyzing/logic/analyzing_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -7,14 +5,26 @@ class AnalyzingProvider extends ChangeNotifier {
   XFile ?selctedimage;
 
   pickGalleryImage()async{
-    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    var image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 800,
+      maxHeight: 800,
+    );
     selctedimage=image;
+    image=null;
     notifyListeners();
   }
 
   pickCameraImage()async{
-    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+    var image = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 800,
+      maxHeight: 800,
+    );
     selctedimage=image;
+    image=null;
     notifyListeners();
   }
   
