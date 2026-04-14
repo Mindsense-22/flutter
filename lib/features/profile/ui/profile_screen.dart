6bdb012bf358ab7a8e54ab1c044309c3388ac9cc
yousifmohamed74 <joe94574@gmail.com>
2026-04-14 16:30:32 +0200

    when pop back to home screen

--- lib/features/profile/ui/profile_screen.dart
+++ lib/features/profile/ui/profile_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mindsense_app/features/main_nav/logic/mainscreenprovider.dart';
 import 'package:mindsense_app/features/profile/ui/widgets/about_user_wid.dart';
 import 'package:mindsense_app/features/profile/ui/widgets/favourite_wid.dart';
 import 'package:mindsense_app/features/profile/ui/widgets/general_settings_wid.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -11,40 +15,48 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "My Profile",
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 20.sp,
-          ),
-        ),
-        actions: [          
-          InkWell(
-            onTap: () {},
-            borderRadius: BorderRadius.circular(120.r),            
-            child: SvgPicture.asset(
-              "assets/images/settings_icon_white.svg",
-              width: 26.w,
-              height: 26.h,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        var provider=context.read<Mainscreenprovider>();
+        provider.changeIndex(0);
+        log(provider.index.toString());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "My Profile",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 20.sp,
             ),
           ),
-          SizedBox(width: 20.w,)
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal:  20.w),
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              AboutUserWid(),
-              GeneralSettingsWid(),
-              FavouriteWid(),
-              SizedBox(height: 30),
-            ],
+          actions: [          
+            InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(120.r),            
+              child: SvgPicture.asset(
+                "assets/images/settings_icon_white.svg",
+                width: 26.w,
+                height: 26.h,
+              ),
+            ),
+            SizedBox(width: 20.w,)
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal:  20.w),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                AboutUserWid(),
+                GeneralSettingsWid(),
+                FavouriteWid(),
+                SizedBox(height: 30),
+              ],
+            ),
           ),
         ),
       ),
