d88ef2e924791ff6a3be79e88d6429adab7d25c6
yousifmohamed74 <joe94574@gmail.com>
2026-04-16 19:13:40 +0200

    inhance profile screen

--- lib/features/profile/ui/widgets/about_user_wid.dart
+++ lib/features/profile/ui/widgets/about_user_wid.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -12,39 +13,64 @@ class AboutUserWid extends StatelessWidget {
       children: [
         SizedBox(height: 18.h),
         Stack(
-          alignment: Alignment.bottomRight,
+          alignment: Alignment.center,
           children: [
             Container(
               width: 110.w,
               height: 110.w,
               decoration: BoxDecoration(
-                color: Color(0xff2DD4BF).withValues(alpha: .06),
-                shape: BoxShape.circle,
+                color: Color(0xff2DD4BF).withValues(alpha: .2),
+                borderRadius: BorderRadius.circular(108.r),
                 border: Border.all(
-                  color: AppColers.primaryColor.withValues(alpha: .5),
-                  width: 1.2.w,
-                ),
-                
-              ),
+                  color: AppColers.primaryColor.withValues(alpha: .3),
+                  width: 2.w,
+                ),                
+              ),              
             ),
             Positioned(
-              bottom: 4.w,
-              right: 4.w,
+              top: 7,
               child: Container(
-                padding: EdgeInsets.all(6.w),
-                decoration: BoxDecoration(
-                  color: AppColers.primaryColor,
-                  shape: BoxShape.circle,
+                width: 98.w,
+                height: 98.w,
+                decoration: BoxDecoration(                
+                borderRadius: BorderRadius.circular(108.r),                                
+              ),
+                child: CachedNetworkImage(
+                    imageUrl: "https://drive.google.com/uc?export=download&id=1HQGGxju316dlVBAE5NkTzAa5drUkEZDm",
+                    fit: BoxFit.fill,                    
+                  ),
+              ),            
+            ),
+            
+            Positioned(
+              bottom: -2.w,
+              right: -1.w,
+              child: InkWell(
+                onTap: () {
+                  
+                },
+                borderRadius: BorderRadius.circular(150.r),
+                splashColor: Colors.grey,
+                child: Container(
+                  padding: EdgeInsets.all(6.w),
+                  decoration: BoxDecoration(
+                    color: AppColers.primaryColor,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Color(0xff0B0F19),
+                      width: 2.w,
+                    ), 
+                  ),
+                  child: SvgPicture.asset("assets/images/camera_icon_black.svg",
+                    height: 16.h,
+                    width: 16.w,
+                  )
                 ),
-                child: SvgPicture.asset("assets/images/camera_icon_black.svg",
-                  height: 16.h,
-                  width: 16.w,
-                )
               ),
             ),
           ],
         ),
-        SizedBox(height: 16.h),
+        SizedBox(height: 14.h),
         Text(
           "Ammar Elmihy",
           style: TextStyle(
