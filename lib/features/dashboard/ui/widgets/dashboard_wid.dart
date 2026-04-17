bf2155b0a5874fb71e33a77050cd24cec8f29a14
yousifmohamed74 <joe94574@gmail.com>
2026-04-17 16:53:53 +0200

    graphical dashboard setup

---
+++ lib/features/dashboard/ui/widgets/dashboard_wid.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardWid extends StatelessWidget {
+  const DashboardWid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
